69ae958181d1a9fb94308b3d3151c9d7cc138dfd
Huido Choo <136903750+Monkshark@users.noreply.github.com>
2024-12-04 13:54:40 +0900

    Update ApiKey.java

--- src/main/java/com/example/upbitcoinautosell/upbitAPI/ApiKey.java
+++ src/main/java/com/example/upbitcoinautosell/upbitAPI/ApiKey.java
@@ -16,4 +16,3 @@ public class ApiKey {
     }
 }
 
-//https://antony-can-do.tistory.com/entry/Spring-Project-%EC%BD%94%EC%9D%B8-%EC%9E%90%EB%8F%99%EB%A7%A4%EB%A7%A4-%ED%94%84%EB%A1%9C%EA%B7%B8%EB%9E%A8-%EB%A7%8C%EB%93%A4%EA%B8%B0-3-%EC%97%85%EB%B9%84%ED%8A%B8-%EB%A7%A4%EC%88%98-%EB%A7%A4%EB%8F%84
